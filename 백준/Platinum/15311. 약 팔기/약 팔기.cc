b5a5159b40e90a9b9d3dd75a0990d9e1ae80daee
h0stuck <97787861+h0stuck@users.noreply.github.com>
2023-06-13 14:35:44 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+int main(){
+    fast_io;
+    cout << 2000 << endl;
+    for(int i=1; i<=1000; i++){
+        cout << 1 << " ";
+    }
+    for(int i=1; i<=1000; i++){
+        cout << 1000 << " ";
+    }
+}

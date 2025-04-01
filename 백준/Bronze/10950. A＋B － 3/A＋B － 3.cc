5ebfff1ddad7d37058285e77cad21d6e9182f640
h0stuck <97787861+h0stuck@users.noreply.github.com>
2025-04-01 15:23:27 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+
+int main(){
+    fast_io;
+    int n;
+    cin >> n;
+    while(n--){
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    }
+
+    return 0;
+
+}

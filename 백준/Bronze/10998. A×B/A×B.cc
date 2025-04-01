39807b661b3d126866656670c5740fd85fd6abd0
h0stuck <97787861+h0stuck@users.noreply.github.com>
2025-04-01 15:19:36 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+
+int main(){
+    fast_io;
+
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
+
+    return 0;
+
+}

454f99ed64e279484b14ae061d966f74bde54e2e
h0stuck <97787861+h0stuck@users.noreply.github.com>
2025-04-01 15:17:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+
+int main(){
+    fast_io;
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+
+    return 0;
+
+}

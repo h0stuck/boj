c1b4ccfffdbc415d6c7c46117d332f03dd4db94e
h0stuck <97787861+h0stuck@users.noreply.github.com>
2025-04-09 14:06:47 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+int main(){
+    fast_io;
+
+    int n;
+    cin >> n;
+
+    vector<int> v1;
+    vector<int> v2;
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        v1.push_back(a);
+        v2.push_back(a);
+    }
+    
+    int r = v1[0];
+    for(int i=0; i<n; i++){
+        for(int j=i; j>=0; j--){
+            if(v1[j] < v1[i] && v2[j] + v1[i] > v2[i]){
+                v2[i] = v2[j] + v1[i];
+                if(v2[i] > r) r = v2[i];
+            }
+        }
+    }
+
+    cout << r << endl;
+
+    return 0;
+}
+
+

f87d86bbd4903cb48d06d97f0830c4a7f7d37b0f
h0stuck <97787861+h0stuck@users.noreply.github.com>
2026-04-02 13:37:15 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+using ll = long long int;
+
+int main() {
+
+    ll n;
+    cin >> n;
+
+    cout << (int)sqrt(n) << endl;
+
+    return 0;
+}
+
